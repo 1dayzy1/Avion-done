f121698b76d8ac2f0d17defcf787819ee6531aa5
1dayzy1 <dayzy173@gmail.com>
2025-03-29 16:29:46 +0300

    Готовый сайт

---
+++ script.js
@@ -0,0 +1,22 @@
+var score = 1;
+
+
+const button_plus = document.querySelector('.plus');
+const button_minus = document.querySelector('.minus');
+const total = document.querySelector('.total');
+
+button_plus.addEventListener('click', function () {
+    score++;
+    total.textContent = score;
+})
+
+button_minus.addEventListener('click', function () {
+    score = score - 1;
+    total.textContent = score;
+})
+
+
+document.querySelector('.burger').addEventListener('click', function () {
+    this.classList.toggle('active');
+    document.querySelector('.navigation').classList.toggle('open');
+})
